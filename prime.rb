ba30c76941b848cc28b7f88e09ead6f7ca452d42
Ramona M <r1mari@yahoo.com>
2018-06-29 11:04:54 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,19 +1,36 @@
 # Add  code here!
-def prime?(number)
+# def prime?(number)
 
-  result = nil
+#   result = nil
 
-  for index in 2..number-1
+#   for index in 2..number-1
 
-    #if number % index == 0
-      if number <= -1 || number % index == 0
-      return result = false
-    else
-      result = true
+#     #if number % index == 0
+#       if number <= -1 || number % index == 0
+#       return result = false
+#     else
+#       result = true
+#     end
+
+#   end
+
+#     result
+
+# end
+
+def prime?(*nums)
+i = 2
+    nums.each do |num|
+        while i < num
+            is_divisible = ((num % i) == 0)
+                if is_divisible == false
+                    x = "#{num}: is NOT a prime number." #false
+               else
+                    x = "#{num}: is a prime number." #true
+                end
+
+            i +=1   
+        end
+        return x
     end
-
-  end
-
-    result
-
 end
