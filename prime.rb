5e5abe4c700cc8ab738718d7501be8d04e7b4cfc
Ramona M <r1mari@yahoo.com>
2018-06-29 11:11:47 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -18,11 +18,11 @@
 
 # end
  def prime?(x):
-    if x < 2:
+    if x < 2
         return False
-    else:
-        for n in range(2,x):
-            if x % n == 0:
+    else
+        for n in range(2,x)
+            if x % n == 0
                return False
         return True
 end
