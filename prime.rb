269648e7d9e6869c7762a92b388342a846a3d1cb
Ramona M <r1mari@yahoo.com>
2018-06-29 11:08:11 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,20 +1,29 @@
 # Add  code here!
-def prime?(number)
+# def prime?(number)
 
-  result = nil
+#   result = nil
 
-  for index in 2..number-1
+#   for index in 2..number-1
 
-    #if number % index == 0
-      if number <= -1 || number % index == 0
-      return result = false
-    else
-      result = true
+#     #if number % index == 0
+#       if number <= -1 || number % index == 0
+#       return result = false
+#     else
+#       result = true
+#     end
+
+#   end
+
+#     result
+
+# end
+ def is_prime?(num)
+    flag = true
+    for x in 2..num-1
+        if num % x == 0
+            flag = false
+        end
     end
-
-  end
-
-    result
-
+    return flag
 end
 
