b20b1f615a0407e5bd0625b1d00aa73e046c2050
Ramona M <r1mari@yahoo.com>
2018-06-29 11:02:04 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -6,7 +6,7 @@ def prime?(number)
   for index in 2..number-1
 
     #if number % index == 0
-      if number<=1 || number%index == 0
+      if number <= 1 || number % index == 0
       return result = false
     else
       result = true
